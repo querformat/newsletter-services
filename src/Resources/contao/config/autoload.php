81a2dfc7899aa1669623805cf2effd5d68c86aba
Enrico Schiller <schiller@querformat.info>
2019-10-16 15:42:43 +0200

    added dca

---
+++ src/Resources/contao/config/autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @package   querformat/newsletter-services
+ * @author    Enrico Schiller
+ * @license   GPL-3.0+
+ * @copyright querformat GmbH & Co. KG
+ */
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'newsletterServices',
+));
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+));
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @package   querformat/newsletter-services
+ * @author    Enrico Schiller
+ * @license   GPL-3.0+
+ * @copyright querformat GmbH & Co. KG
+ */
