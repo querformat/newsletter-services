30914be277e7d96b7dc5de610fa0de5733721fd1
Enrico Schiller <schiller@querformat.info>
2019-10-17 13:46:03 +0200

    test

---
+++ src/newsletter-services/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @package   querformat/insert-translations
+ * @author    Enrico Schiller
+ * @license   GPL-3.0+
+ * @copyright querformat GmbH & Co. KG
+ */
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'newsletterServices',
+));
+
